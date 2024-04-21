1bfdba65bf4d57219919c2069879847163293984
Mohammad <mohammadkhakharia@gmail.com>
2024-04-21 16:18:30 -0400

    pass stage

--- app/server.go
+++ app/server.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
+
+type Type byte
+
+const (
+	Integer = ':'
+	String  = '+'
+	Bulk    = '$'
+	Array   = '*'
+	Error   = '-'
+)
+
+type RESP struct {
+	Type  Type
+	Raw   []byte
+	Data  []byte
+	Count int
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -28,22 +47,155 @@ func main() {
 
 }
 
+func (r RESP) Bytes() []byte {
+	return r.Data
+}
+
+func (r RESP) String() string {
+	return string(r.Data)
+}
+
+func (r RESP) Int() int64 {
+	x, _ := strconv.ParseInt(r.String(), 10, 64)
+	return x
+}
+
+func (r RESP) Float() float64 {
+	x, _ := strconv.ParseFloat(r.String(), 10)
+	return x
+}
+
+func ReadNextRESP(b []byte) (n int, resp RESP) {
+	if len(b) == 0 {
+		return 0, RESP{} // no data to read
+	}
+	resp.Type = Type(b[0])
+	switch resp.Type {
+	case Integer, String, Bulk, Array, Error:
+	default:
+		return 0, RESP{} // invalid kind
+	}
+	// read to end of line
+	i := 1
+	for ; ; i++ {
+		if i == len(b) {
+			return 0, RESP{} // not enough data
+		}
+		if b[i] == '\n' {
+			if b[i-1] != '\r' {
+				return 0, RESP{} //, missing CR character
+			}
+			i++
+			break
+		}
+	}
+	resp.Raw = b[0:i]
+	resp.Data = b[1 : i-2]
+	if resp.Type == Integer {
+		// Integer
+		if len(resp.Data) == 0 {
+			return 0, RESP{} //, invalid integer
+		}
+		var j int
+		if resp.Data[0] == '-' {
+			if len(resp.Data) == 1 {
+				return 0, RESP{} //, invalid integer
+			}
+			j++
+		}
+		for ; j < len(resp.Data); j++ {
+			if resp.Data[j] < '0' || resp.Data[j] > '9' {
+				return 0, RESP{} // invalid integer
+			}
+		}
+		return len(resp.Raw), resp
+	}
+	if resp.Type == String || resp.Type == Error {
+		// String, Error
+		return len(resp.Raw), resp
+	}
+	var err error
+	resp.Count, err = strconv.Atoi(string(resp.Data))
+	if resp.Type == Bulk {
+		// Bulk
+		if err != nil {
+			return 0, RESP{} // invalid number of bytes
+		}
+		if resp.Count < 0 {
+			resp.Data = nil
+			resp.Count = 0
+			return len(resp.Raw), resp
+		}
+		if len(b) < i+resp.Count+2 {
+			return 0, RESP{} // not enough data
+		}
+		if b[i+resp.Count] != '\r' || b[i+resp.Count+1] != '\n' {
+			return 0, RESP{} // invalid end of line
+		}
+		resp.Data = b[i : i+resp.Count]
+		resp.Raw = b[0 : i+resp.Count+2]
+		resp.Count = 0
+		return len(resp.Raw), resp
+	}
+	// Array
+	if err != nil {
+		return 0, RESP{} // invalid number of elements
+	}
+	var tn int
+	sdata := b[i:]
+	for j := 0; j < resp.Count; j++ {
+		rn, rresp := ReadNextRESP(sdata)
+		if rresp.Type == 0 {
+			return 0, RESP{}
+		}
+		tn += rn
+		sdata = sdata[rn:]
+	}
+	resp.Data = b[i : i+tn]
+	resp.Raw = b[0 : i+tn]
+	return len(resp.Raw), resp
+}
+
+func parseMsg(msg []byte) error {
+	s, resp := ReadNextRESP(msg)
+	if s == 0 {
+		return errors.New("no resp object")
+	}
+	t := resp.Type
+	fmt.Println(resp)
+	switch t {
+	case Integer:
+	case String:
+	case Bulk:
+	case Array:
+		handleCommand(resp)
+	case Error:
+	}
+	return nil
+}
+
+func handleCommand(resp RESP) {
+
+}
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		buff := make([]byte, 1024)
+		_, err := conn.Read(buff)
 
 		if err != nil {
 			fmt.Println("Failed to read buffer", err)
 			os.Exit(1)
 		}
-		str := "+PONG\r\n"
-		conn.Write([]byte(str))
+
+		err = parseMsg(buff)
+
 		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			return
+			fmt.Println("Error reading resp")
+			os.Exit(1)
 		}
+
 	}
 
 }
