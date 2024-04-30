0bb61cd8fe1b381485bd133ba3bef7a9e032167e
Mohammad <mohammadkhakharia@gmail.com>
2024-04-29 20:44:23 -0400

    pass stage

--- app/commands.go
+++ app/commands.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/hex"
 	"net"
 	"strconv"
 	"strings"
@@ -92,9 +92,12 @@ func (s *Server) psync(cmd []RESP) []byte {
 
 func (s *Server) rdbTransfer(conn net.Conn) {
 	rawHex := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-	i, err := strconv.ParseUint(rawHex, 16, 64)
+
+	data, err := hex.DecodeString(rawHex)
 	if err != nil {
-		fmt.Printf("%s", err)
+		return
 	}
-	fmt.Printf("%064b\n", i)
+	response := "$" + strconv.Itoa(len(data)) + "\r\n"
+	conn.Write([]byte(response))
+	conn.Write(data)
 }
